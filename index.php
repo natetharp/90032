5232d1fe2c008c59775c31042cc6098eed7f2c39
Nate Tharp <nateisbusy@gmail.com>
2014-05-20 12:45:44 -0700

    Stacey
    Replacing Pico CMS with Stacey CMS

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 
-define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
-define('CONTENT_DIR', ROOT_DIR .'content/');
-define('CONTENT_EXT', '.md');
-define('LIB_DIR', ROOT_DIR .'lib/');
-define('PLUGINS_DIR', ROOT_DIR .'plugins/');
-define('THEMES_DIR', ROOT_DIR .'themes/');
-define('CACHE_DIR', LIB_DIR .'cache/');
+# let people know if they are running an unsupported version of PHP
+if(phpversion() < 5) {
+  
+  die('<h3>Stacey requires PHP/5.0 or higher.<br>You are currently running PHP/'.phpversion().'.</h3><p>You should contact your host to see if they can upgrade your version of PHP.</p>');
 
-require_once(ROOT_DIR .'vendor/autoload.php');
-require_once(LIB_DIR .'pico.php');
-$pico = new Pico();
+} else {
+
+  # require helpers class so we can use rglob
+  require_once './app/helpers.inc.php';
+  # include any php files which sit in the app folder
+  foreach(Helpers::rglob('./app/**.inc.php') as $include) include_once $include;
+
+  # start the app
+  new Stacey($_GET);
+  
+}
+
+?>
